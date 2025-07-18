740956a59afc77926d76cb5ac360619345c3d1a6
jrobo-gith <jacktheroberts10@gmail.com>
2025-07-18 16:04:50 +0100

    Added non-newtonian disjoining pressure preliminary tests

--- tests.py
+++ tests.py
@@ -1,9 +1,8 @@
 import numpy as np
-rows = 2
-cols = 4
 
-j = 0
-for row in range(rows):
-    for col in range(cols):
-        j += 1
-        print(j)
+x = np.linspace(0, 10, 100)
+
+print(np.all(x < 10.1))
+
+
+
